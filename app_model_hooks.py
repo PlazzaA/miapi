6a1cc00601abff00c3d6eaa7518a7c1ca50c7e1f
PlazzaA <plazzagarciaalejandro@gmail.com>
2025-05-26 10:38:40 +0200

    last

--- app_model_hooks.py
+++ app_model_hooks.py
@@ -19,7 +19,7 @@ app.config["DEBUG"] = True
 # Enruta la landing page (endpoint /)
 @app.route("/", methods=["GET"])
 def hello():
-    return "Bienvenido a mi API del modelo advertising"
+    return "Bienvenido a mi API del modelo raro con muy mal predict"
 
 
 # Enruta la funcion al endpoint /api/v1/predict
